ebf3d717995a586429ff1e6a8cea0791afa136bd
Guillaume Renard <guillaume.renard@altusgroup.com>
2023-03-10 10:17:27 +0100

    use Banner in MFE1 App

--- mfe1/src/App.tsx
+++ mfe1/src/App.tsx
@@ -1,7 +1,15 @@
+import { FusionThemeProvider } from "fusion";
 import "./App.css";
+import Banner from "./components/Banner";
 
 function App() {
-  return <div className="App">MFE 1</div>;
+  return (
+    <div className="App">
+      <FusionThemeProvider>
+        <Banner />
+      </FusionThemeProvider>
+    </div>
+  );
 }
 
 export default App;
